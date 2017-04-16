8f36cae28455d87c2878fa8c1eeb8b96b6ac14bc
姜康 <jiangkang@bestpay.com.cn>
2017-04-16 21:18:43 +0800

    KNN算法实现手写数字识别

---
+++ KNN_DigitsRecognition/file2img.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 将txt像素数据集生成图片数据集
+
+import os
+from PIL import Image
+
+
+# 将txt文件转换为png图片
+def file2img(filename, target):
+    fr = open(filename)
+    name = filename.split('/')[-1][:-4]
+    print(name)
+    image = Image.new("L", (32, 32))
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            colorValue = int(lineStr[j])
+            if colorValue == 1:
+                colorValue = 255
+            image.putpixel((j, i), int(colorValue))
+            image.save(target + "/" + name + ".png")
+
+
+# 生成图片
+def genImg(filename, target):
+    fileList = os.listdir(filename)
+    m = len(fileList)
+    if not os.path.exists(target):
+        os.mkdir(target)
+    for i in range(m):
+        file2img(filename + fileList[i], target)
+
+
+genImg("knn-digits/trainingDigits/", "training_img")
+genImg("knn-digits/testDigits/", "test_img")
+
+os.system("say '程序执行完毕'")
---
+++ KNN_DigitsRecognition/knn.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 14:53:46 2017
+
+@author: jiangkang
+"""
+
+import numpy
+import operator
+import os
+import matplotlib.pyplot as plt
+
+
+# 将图像数据转换为（1，1024）矩阵
+def img2vector(filename):
+    returnVect = numpy.zeros((1, 1024))
+    file = open(filename)
+    for i in range(32):
+        lineStr = file.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+
+# kNN分类器
+def classifier(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = numpy.tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+# 测试手写数字识别代码
+def handWritingClassTest(k):
+    hwLabels = []
+    trainingFileList = os.listdir('knn-digits/trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = numpy.zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector("knn-digits/trainingDigits/%s" % fileNameStr)
+    testFileList = os.listdir('knn-digits/testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorTest = img2vector("knn-digits/testDigits/%s" % fileNameStr)
+        result = classifier(vectorTest, trainingMat, hwLabels, k)
+        print("分类结果是：%d, 真实结果是：%d" % (result, classNumStr))
+        if result != classNumStr:
+            errorCount += 1.0
+    print("错误总数：%d" % errorCount)
+    print("错误率：%f" % (errorCount / mTest))
+    return errorCount
+
+
+# 这里是为了测试取不同的k值，识别的效果如何
+def selectK():
+    x = list()
+    y = list()
+    for i in range(1, 5):
+        x.append(int(i))
+        y.append(int(handWritingClassTest(i)))
+    plt.plot(x, y)
+    # 由于程序执行时间比较长，这里在程序执行完毕时进行语音提醒（针对Mac，Windows用户去除该行代码）
+    os.system("say '程序执行完毕'")
+    plt.show()
+
+
+# 开始测试，会生成折线图
+selectK()
+
+# 测试证明，k选3效果比较号
+handWritingClassTest(3)
